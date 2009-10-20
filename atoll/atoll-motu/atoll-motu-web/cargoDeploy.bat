c7b8aafa10527c8f47b1183d11efe4194858f4e3
dearith <dearith@4fb39819-1ace-417d-baf3-7cd5f5b47953>
2009-10-20 13:33:02 +0000

    Modification pour WPS
    git-svn-id: http://svn.code.sf.net/p/cls-motu/code/trunk@1319 4fb39819-1ace-417d-baf3-7cd5f5b47953

--- atoll/atoll-motu/atoll-motu-web/cargoDeploy.bat
+++ atoll/atoll-motu/atoll-motu-web/cargoDeploy.bat
@@ -9,7 +9,7 @@ set CURRENT_DIR=%cd%
 cd ..
 
 REM echo validate atoll-motu and  dependencies
-REM call mvn -Dmaven.test.skip=true validate -P %1
+call mvn -Dmaven.test.skip=true validate -P %1
 
 echo install atoll-motu and  dependencies
 call mvn -Dmaven.test.skip=true install
