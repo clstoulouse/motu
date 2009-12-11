88a87fae6e274058f3784afd3e5c6e9fef308c00
dearith <dearith@4fb39819-1ace-417d-baf3-7cd5f5b47953>
2009-12-11 15:19:58 +0000

    Ajout config de d�ploiement
    git-svn-id: http://svn.code.sf.net/p/cls-motu/code/trunk@1490 4fb39819-1ace-417d-baf3-7cd5f5b47953

--- atoll/atoll-motu/atoll-motu-web/deployMotu.bat
+++ atoll/atoll-motu/atoll-motu-web/deployMotu.bat
@@ -1,5 +1,4 @@
-
-rem call ant deploy-dev-atoll-motu
+REM call ant deploy-dev-atoll-motu
 
 REM call ant deploy-localhost
 
@@ -10,3 +9,11 @@ REM call ant deploy-production-aviso-motu
 REM call ant deploy-production-mercator-motu
 
 REM call ant deploy-production-themismotu
+REM call ant deploy-validation-themismotu
+
+REM call ant deploy-production-mercator-data1
+REM call ant deploy-production-mercator-data1-myocean
+
+REM call ant deploy-production-catsat-data1
+
+REM call ant deploy-production-aviso-data1
