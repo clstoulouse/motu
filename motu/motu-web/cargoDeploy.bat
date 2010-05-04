0c389f9a518a843fd831cd8e14019364756bc8b9
dearith <dearith@4fb39819-1ace-417d-baf3-7cd5f5b47953>
2010-05-04 08:43:06 +0000

    Rajout Cargo pour deploiement facile en dev
    git-svn-id: http://svn.code.sf.net/p/cls-motu/code/trunk@2773 4fb39819-1ace-417d-baf3-7cd5f5b47953

--- motu/motu-web/cargoDeploy.bat
+++ motu/motu-web/cargoDeploy.bat
@@ -3,15 +3,31 @@
 echo off
 
 set CURRENT_DIR=%cd%
+REM echo %CURRENT_DIR%
+goto deploy
 
-:install-motu
+:install-motu-api-message
+cd %CURRENT_DIR%\..\motu-api\motu-api-message
+call mvn -Dmaven.test.skip=true -P %1 clean install 
 
-cd ..
+:install-motu-api-rest
+cd %CURRENT_DIR%\..\motu-api\motu-api-rest
+call mvn -Dmaven.test.skip=true -P %1 clean install 
 
-REM echo validate atoll-motu and  dependencies
-REM call mvn -Dmaven.test.skip=true -P %1 validate 
+:install-motu-library-converter
+cd %CURRENT_DIR%\..\motu-library\motu-library-converter
+call mvn -Dmaven.test.skip=true -P %1 clean install 
 
-REM echo install atoll-motu and  dependencies
+:install-motu-library-inventory
+cd %CURRENT_DIR%\..\motu-library\motu-library-inventory
+call mvn -Dmaven.test.skip=true -P %1 clean install 
+
+:install-motu-library-misc
+cd %CURRENT_DIR%\..\motu-library\motu-library-misc
+call mvn -Dmaven.test.skip=true -P %1 clean install 
+
+:install-motu-processor-wps
+cd %CURRENT_DIR%\..\motu-processor\motu-processor-wps
 call mvn -Dmaven.test.skip=true -P %1 clean install 
 
 cd /D %CURRENT_DIR%
@@ -21,9 +37,9 @@ call mvn -Dmaven.test.skip=true -P %1 clean install
 
 :deploy
 
-echo undeploy atoll-motu-web with profile '%1'
+echo undeploy motu-web with profile '%1'
 call mvn -Dmaven.test.skip=true -P %1 cargo:deployer-undeploy 
-echo deploy atoll-motu-web with profile '%1'
+echo deploy motu-web with profile '%1'
 call mvn -Dmaven.test.skip=true -P %1 clean package  cargo:deployer-deploy 
 
 
