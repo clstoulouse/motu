9aa55c4e0ff53df9abb156739dd524385f8be23a
dearith <dearith@4fb39819-1ace-417d-baf3-7cd5f5b47953>
2010-05-19 09:33:09 +0000

    Prepare distribution
    git-svn-id: http://svn.code.sf.net/p/cls-motu/code/trunk@3163 4fb39819-1ace-417d-baf3-7cd5f5b47953

--- motu/motu-web/cargoDeploy.bat
+++ motu/motu-web/cargoDeploy.bat
@@ -5,54 +5,60 @@ echo off
 set CURRENT_DIR=%cd%
 REM echo %CURRENT_DIR%
 
-set WHAT=%2
+REM set PROFILE=%1
+set PROFILE=
+REM set PROFILE=-P localhost,onlybinaries
+
+REM set WHAT=%2
+set WHAT=install
+
 
 echo %WHAT%
 
-if %WHAT% == "deploy" goto deploy
+if %WHAT% == deploy goto deploy
 
 :install-motu-api-message
 cd %CURRENT_DIR%\..\motu-api\motu-api-message
-call mvn -Dmaven.test.skip=true -P %1 clean install 
+call mvn -Dmaven.test.skip=true %PROFILE% clean install 
 
 :install-motu-api-rest
 cd %CURRENT_DIR%\..\motu-api\motu-api-rest
-call mvn -Dmaven.test.skip=true -P %1 clean install 
+call mvn -Dmaven.test.skip=true %PROFILE% clean install 
 
 :install-motu-library-converter
 cd %CURRENT_DIR%\..\motu-library\motu-library-converter
-call mvn -Dmaven.test.skip=true -P %1 clean install 
+call mvn -Dmaven.test.skip=true %PROFILE% clean install 
 
 :install-motu-library-inventory
 cd %CURRENT_DIR%\..\motu-library\motu-library-inventory
-call mvn -Dmaven.test.skip=true -P %1 clean install 
+call mvn -Dmaven.test.skip=true %PROFILE% clean install 
 
 :install-motu-library-misc
 cd %CURRENT_DIR%\..\motu-library\motu-library-misc
-call mvn -Dmaven.test.skip=true -P %1 clean install 
+call mvn -Dmaven.test.skip=true %PROFILE% clean install 
 
 :install-motu-processor-wps
 cd %CURRENT_DIR%\..\motu-processor\motu-processor-wps
-call mvn -Dmaven.test.skip=true -P %1 clean install 
+call mvn -Dmaven.test.skip=true %PROFILE% clean install 
 
 :install-motu-api-client
 cd %CURRENT_DIR%\..\motu-api\motu-api-client
-REM call mvn -Dmaven.test.skip=true -P %1 clean install 
-call mvn -Dmaven.test.skip=true -P %1 assembly:assembly
+REM call mvn -Dmaven.test.skip=true %PROFILE% clean install 
+call mvn -Dmaven.test.skip=true %PROFILE% assembly:assembly
 
 cd /D %CURRENT_DIR%
 :install-motu-web
 REM echo install atoll-motu-web
-call mvn -Dmaven.test.skip=true -P %1 clean install 
+call mvn -Dmaven.test.skip=true %PROFILE% clean install 
 
-if %WHAT% == "install" goto end
+if %WHAT% == install goto end
 
 :deploy
 
-echo undeploy motu-web with profile '%1'
-call mvn -Dmaven.test.skip=true -P %1 cargo:deployer-undeploy 
-echo deploy motu-web with profile '%1'
-call mvn -Dmaven.test.skip=true -P %1 clean package  cargo:deployer-deploy 
+echo undeploy motu-web with profile '%PROFILE%'
+call mvn -Dmaven.test.skip=true %PROFILE% cargo:deployer-undeploy 
+echo deploy motu-web with profile '%PROFILE%'
+call mvn -Dmaven.test.skip=true %PROFILE% clean package  cargo:deployer-deploy 
 
 
 :end
