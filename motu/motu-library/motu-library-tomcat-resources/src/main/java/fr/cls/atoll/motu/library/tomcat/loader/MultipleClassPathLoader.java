133b1cb2017e4d537e4933ee71142302599bcacc
ccamel <ccamel@4fb39819-1ace-417d-baf3-7cd5f5b47953>
2010-07-19 15:50:28 +0000

    Performs variable substitution
    git-svn-id: http://svn.code.sf.net/p/cls-motu/code/trunk@3480 4fb39819-1ace-417d-baf3-7cd5f5b47953

--- motu/motu-library/motu-library-tomcat-resources/src/main/java/fr/cls/atoll/motu/library/tomcat/loader/MultipleClassPathLoader.java
+++ motu/motu-library/motu-library-tomcat-resources/src/main/java/fr/cls/atoll/motu/library/tomcat/loader/MultipleClassPathLoader.java
@@ -1,5 +1,7 @@
 package fr.cls.atoll.motu.library.tomcat.loader;
 
+import fr.cls.atoll.motu.library.tomcat.util.VariableSubstitutionUtils;
+
 import java.io.File;
 import java.net.URI;
 import java.util.StringTokenizer;
@@ -14,6 +16,16 @@ public class MultipleClassPathLoader extends WebappLoader {
      * <code>;</code> separated list of additional path elements.
      */
     private String virtualClasspath;
+
+    /**
+     * Separator to use.
+     */
+    private String separator = ";";
+
+    /**
+     * Whether or not check the existence of variables.
+     */
+    private boolean checkVariables = true;
 
     /**
      * Construct a new WebappLoader with no defined parent class loader (so that the actual parent will be the
@@ -45,9 +57,11 @@ public class MultipleClassPathLoader extends WebappLoader {
 
     @Override
     public void start() throws LifecycleException {
+        // Do variable substitution if necessary
+
         // just add any jar/directory set in virtual classpath to the
         // repositories list before calling start on the standard WebappLoader
-        StringTokenizer tkn = new StringTokenizer(virtualClasspath, ";");
+        StringTokenizer tkn = new StringTokenizer(VariableSubstitutionUtils.substituteString(virtualClasspath, isCheckVariables()), getSeparator());
         while (tkn.hasMoreTokens()) {
             File file = new File(tkn.nextToken());
             if (!file.exists()) {
@@ -69,5 +83,40 @@ public class MultipleClassPathLoader extends WebappLoader {
         this.setLoaderClass(CustomWebAppClassLoader.class.getCanonicalName());
 
         super.start();
+    }
+
+    /**
+     * @return the separator
+     */
+    public String getSeparator() {
+        return separator;
+    }
+
+    /**
+     * @param separator the separator to set
+     */
+    public void setSeparator(String separator) {
+        this.separator = separator;
+    }
+
+    /**
+     * @return the checkVariables
+     */
+    public boolean isCheckVariables() {
+        return checkVariables;
+    }
+
+    /**
+     * @param checkVariables the checkVariables to set
+     */
+    public void setCheckVariables(boolean checkVariables) {
+        this.checkVariables = checkVariables;
+    }
+
+    /**
+     * @return the virtualClasspath
+     */
+    public String getVirtualClasspath() {
+        return virtualClasspath;
     }
 }
