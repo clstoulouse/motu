bcc7f1a5d28160d9b820603ce46ad4430f9c327b
dearith <dearith@4fb39819-1ace-417d-baf3-7cd5f5b47953>
2010-11-24 17:47:24 +0000

    add test
    git-svn-id: http://svn.code.sf.net/p/cls-motu/code/trunk@3607 4fb39819-1ace-417d-baf3-7cd5f5b47953

--- motu/motu-library/motu-library-misc/src/test/java/fr/cls/atoll/motu/library/misc/intfce/TestIntfce.java
+++ motu/motu-library/motu-library-misc/src/test/java/fr/cls/atoll/motu/library/misc/intfce/TestIntfce.java
@@ -378,7 +378,7 @@ public class TestIntfce {
         // productInformation();
         // productInformationFromLocationData();
         // productExtractDataMersea();
-        productDownloadInfo();
+        //productDownloadInfo();
         // productExtractDataHTMLAviso();
         // productExtractDataAviso();
         // productExtractDataAvisofromProductId();
@@ -423,6 +423,7 @@ public class TestIntfce {
 //        productExtractXYTopaz2();
 //        productExtractXYTopaz3();
 //        productExtractXYMetNo1();
+          productExtractAcri();
 
     }
 
@@ -3151,5 +3152,74 @@ public class TestIntfce {
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-    }    
+    }   
+    
+    public static void productExtractAcri() {
+        String locationData = "http://myocean-ftp.acri.fr:8080/thredds/dodsC/dataset-oc-acri-global-100km-chl-daily-ran";
+
+        // productInformationFromLocationData(locationData);
+
+        List<String> listVar = new ArrayList<String>();
+        // add variable to extract
+        // listVar.add("salinity");
+        // listVar.add("u");
+        //listVar.add("temperature");
+        // listVar.add("longitude");
+        // listVar.add("latitude");
+
+        // add temporal criteria
+        // first element is start date
+        // second element is end date (optional)
+        // if only start date is set, end date equals start date
+        List<String> listTemporalCoverage = new ArrayList<String>();
+        listTemporalCoverage.add("2010-10-25");
+        listTemporalCoverage.add("2010-10-25");
+
+        // add Lat/Lon criteria
+        // first element is low latitude
+        // second element is low longitude
+        // third element is high latitude
+        // fourth element is high longitude
+        List<String> listLatLonCoverage = new ArrayList<String>();
+//        listLatLonCoverage.add("40");
+//        listLatLonCoverage.add("0");
+//        listLatLonCoverage.add("34.68649673461914");
+//        listLatLonCoverage.add("100");
+
+        // add depth (Z) criteria
+        // first element is low depth
+        // second element is high depth (optional)
+        // if only low depth is set, high depth equals low depth value
+        List<String> listDepthCoverage = new ArrayList<String>();
+//        listDepthCoverage.add("5");
+//        listDepthCoverage.add("30");
+
+        Product product = null;
+
+        try {
+            Organizer organizer = new Organizer();
+
+            product = organizer.extractData(locationData,
+                                            listVar,
+                                            listTemporalCoverage,
+                                            listLatLonCoverage,
+                                            listDepthCoverage,
+                                            null,
+                                            Organizer.Format.NETCDF);
+
+            // get the output full file name (with path)
+            String extractLocationData = product.getExtractLocationData();
+            // get the url to download the output file.
+            String urlExtractPath = product.getDownloadUrlPath();
+
+            System.out.println(String.format("Product file is stored on the server in %s", extractLocationData));
+            System.out.println(String.format("Product %s can be downloaded with http at %s", product.getProductId(), urlExtractPath));
+
+        } catch (MotuExceptionBase e) {
+            System.out.println(e.notifyException());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }      
+    
 }
