3a8e2464b020a67ca2133b1cb2ec3aa570459f75
dearith <dearith@4fb39819-1ace-417d-baf3-7cd5f5b47953>
2011-03-31 14:22:56 +0000

    MIS-258: Implement some "Probes" through JMX that exposes:
        * version of motu and its dependencies
        * usefull attributes of the queue server (pending tasks, in process tasks, etc...)
    
    
    git-svn-id: http://svn.code.sf.net/p/cls-motu/code/trunk@3782 4fb39819-1ace-417d-baf3-7cd5f5b47953

---
+++ motu/motu-api/motu-api-message/src/main/java/fr/cls/atoll/motu/api/message/mxbean/StatusModeResponseMXBean.java
@@ -0,0 +1,72 @@
+package fr.cls.atoll.motu.api.message.mxbean;
+
+import javax.xml.datatype.XMLGregorianCalendar;
+
+import fr.cls.atoll.motu.api.message.xml.ErrorType;
+import fr.cls.atoll.motu.api.message.xml.StatusModeType;
+
+public interface StatusModeResponseMXBean {
+    /**
+     * Gets the value of the status property.
+     * 
+     * @return possible object is {@link StatusModeType }
+     * 
+     */
+    public StatusModeType getStatus();
+
+    /**
+     * Gets the value of the requestId property.
+     * 
+     * @return possible object is {@link Long }
+     * 
+     */
+    public Long getRequestId();
+
+    /**
+     * Gets the value of the dateProc property.
+     * 
+     * @return possible object is {@link XMLGregorianCalendar }
+     * 
+     */
+    public XMLGregorianCalendar getDateProc();
+
+    /**
+     * Gets the value of the size property.
+     * 
+     * @return possible object is {@link Double }
+     * 
+     */
+    public Double getSize();
+
+    /**
+     * Gets the value of the remoteUri property.
+     * 
+     * @return possible object is {@link String }
+     * 
+     */
+    public String getRemoteUri();
+
+    /**
+     * Gets the value of the localUri property.
+     * 
+     * @return possible object is {@link String }
+     * 
+     */
+    public String getLocalUri();
+
+    /**
+     * Gets the value of the msg property.
+     * 
+     * @return possible object is {@link String }
+     * 
+     */
+    public String getMsg();
+
+    /**
+     * Gets the value of the code property.
+     * 
+     * @return possible object is {@link ErrorType }
+     * 
+     */
+    public ErrorType getCode();
+}
