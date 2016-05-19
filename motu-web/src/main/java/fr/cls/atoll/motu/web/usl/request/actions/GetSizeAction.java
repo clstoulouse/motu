eda6d647d552ed3a5f4d94157d4ea5b7c931e57b
Pierre Lacoste <pierre.lacoste@cls.fr>
2016-05-19 09:45:39 +0200

    Factorisation of download methods

---
+++ motu-web/src/main/java/fr/cls/atoll/motu/web/usl/request/actions/GetSizeAction.java
@@ -0,0 +1,29 @@
+package fr.cls.atoll.motu.web.usl.request.actions;
+
+import java.io.IOException;
+
+import fr.cls.atoll.motu.web.usl.request.parameter.exception.InvalidHTTPParameterException;
+
+/**
+ * <br><br>Copyright : Copyright (c) 2016
+ * <br><br>Société : CLS (Collecte Localisation Satellites)
+ * @author Pierre LACOSTE
+ * @version $Revision: 1.1 $ - $Date: 2007-05-22 16:56:28 $
+ */
+public class GetSizeAction extends AbstractAction {
+
+    /** {@inheritDoc} */
+    @Override
+    protected void checkHTTPParameters() throws InvalidHTTPParameterException {
+        // TODO Auto-generated method stub
+
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected void process() throws IOException {
+        // TODO Auto-generated method stub
+
+    }
+
+}
