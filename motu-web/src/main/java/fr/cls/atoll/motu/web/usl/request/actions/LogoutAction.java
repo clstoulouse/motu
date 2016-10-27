3c502b3b3ae340a65453519d2009b2f1b5a7e862
Sylvain Marty <smarty@cls.fr>
2016-10-27 17:30:06 +0200

    Fix issue in logout with SSO

--- motu-web/src/main/java/fr/cls/atoll/motu/web/usl/request/actions/LogoutAction.java
+++ motu-web/src/main/java/fr/cls/atoll/motu/web/usl/request/actions/LogoutAction.java
@@ -1,9 +1,13 @@
 package fr.cls.atoll.motu.web.usl.request.actions;
 
+import java.io.IOException;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import fr.cls.atoll.motu.api.message.xml.ErrorType;
+import fr.cls.atoll.motu.web.bll.BLLManager;
 import fr.cls.atoll.motu.web.bll.exception.MotuException;
 import fr.cls.atoll.motu.web.usl.request.parameter.exception.InvalidHTTPParameterException;
 
@@ -41,6 +45,20 @@ public class LogoutAction extends AbstractAction {
     /** {@inheritDoc} */
     @Override
     protected void process() throws MotuException {
-        getSession().invalidate();
+        if (getSession() != null) {
+            getSession().invalidate();
+        }
+        logoutFromCasSSO();
+    }
+
+    private void logoutFromCasSSO() throws MotuException {
+        if (BLLManager.getInstance().getConfigManager().isCasActivated()) {
+            // Redirect to logout fron CAS SSO server
+            try {
+                getResponse().sendRedirect(BLLManager.getInstance().getConfigManager().getCasServerUrl() + "/logout");
+            } catch (IOException e) {
+                throw new MotuException(ErrorType.SYSTEM, "Error while sending download redirection PARAM_MODE_CONSOLE", e);
+            }
+        }
     }
 }
