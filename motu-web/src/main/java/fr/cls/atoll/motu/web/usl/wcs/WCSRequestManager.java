63c29f6a193bcc910a03e562f5e26a85d7f3306f
Sylvain Marty <smarty@cls.fr>
2016-11-17 11:30:03 +0100

    Init WCS2.0 with draft files

---
+++ motu-web/src/main/java/fr/cls/atoll/motu/web/usl/wcs/WCSRequestManager.java
@@ -0,0 +1,25 @@
+package fr.cls.atoll.motu.web.usl.wcs;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * <br>
+ * <br>
+ * Copyright : Copyright (c) 2016 <br>
+ * <br>
+ * Société : CLS (Collecte Localisation Satellites)
+ * 
+ * @author Sylvain MARTY
+ * @version $Revision: 1.1 $ - $Date: 2007-05-22 16:56:28 $
+ */
+public class WCSRequestManager implements IWCSRequestManager {
+
+    /** {@inheritDoc} */
+    @Override
+    public void onNewRequest(HttpServletRequest request, HttpServletResponse response) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
