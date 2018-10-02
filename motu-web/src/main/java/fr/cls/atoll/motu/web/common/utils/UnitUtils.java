32e4e7e6d91a6950910a9fc77af6f4a5a584a8e9
Sylvain Marty <smarty@cls.fr>
2018-10-02 16:28:30 +0200

    Fix unit issue 1024 instead of 1000

--- motu-web/src/main/java/fr/cls/atoll/motu/web/common/utils/UnitUtils.java
+++ motu-web/src/main/java/fr/cls/atoll/motu/web/common/utils/UnitUtils.java
@@ -9,6 +9,12 @@ package fr.cls.atoll.motu.web.common.utils;
  * 
  * @author Sylvain MARTY
  * @version $Revision: 1.1 $ - $Date: 2007-05-22 16:56:28 $
+ * 
+ * 
+ *          There are really 1024 bytes in a kilobyte. The reason for this is because computers are based on
+ *          the binary system. That means hard drives and memory are measured in powers of 2 2^0 = 1, 2^1 = 2,
+ *          2^2 = 4, ..., 2^10 = 1024 Therefore, 2^10, or 1024 bytes compose one kilobyte. 1024*1024=1048576
+ * 
  */
 public class UnitUtils {
 
@@ -28,15 +34,11 @@ public class UnitUtils {
     }
 
     public static Double bytetoKilobyte(double sizeInByte) {
-        return sizeInByte / 1000;
-    }
-
-    public static Long bytetoKilobyte(long sizeInByte) {
-        return sizeInByte / 1000;
+        return sizeInByte / 1024;
     }
 
     public static double byteToMegaByte(double sizeInByte) {
-        return sizeInByte / 1000000;
+        return sizeInByte / 1048576; // 1024*1024=1048576 2^10=1024
     }
 
     public static long kilobyteToByte(long sizeInKilobyte) {
@@ -44,11 +46,11 @@ public class UnitUtils {
     }
 
     public static double kilobyteToMegabyte(double sizeInKilobyte) {
-        return sizeInKilobyte / 1000;
+        return sizeInKilobyte / 1024;
     }
 
     public static double megabyteToByte(double sizeInMegabyte) {
-        return sizeInMegabyte * 1000000;
+        return sizeInMegabyte * 1048576;//
     }
 
 }
