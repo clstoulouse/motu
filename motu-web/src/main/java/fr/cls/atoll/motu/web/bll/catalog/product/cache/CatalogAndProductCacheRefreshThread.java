7117332891b09e4a63b0f3c0812a27b73bbd3008
Sylvain Marty <smarty@cls.fr>
2019-05-09 11:18:04 +0200

    Fix Cache init, and use motu-products-3.11

--- motu-web/src/main/java/fr/cls/atoll/motu/web/bll/catalog/product/cache/CatalogAndProductCacheRefreshThread.java
+++ motu-web/src/main/java/fr/cls/atoll/motu/web/bll/catalog/product/cache/CatalogAndProductCacheRefreshThread.java
@@ -1,13 +1,10 @@
 package fr.cls.atoll.motu.web.bll.catalog.product.cache;
 
-import java.util.List;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import fr.cls.atoll.motu.web.bll.BLLManager;
 import fr.cls.atoll.motu.web.common.thread.StoppableDaemonThread;
-import fr.cls.atoll.motu.web.dal.config.xml.model.ConfigService;
 
 /**
  * Manage the automatique cache refresh. <br>
@@ -24,21 +21,20 @@ public abstract class CatalogAndProductCacheRefreshThread extends StoppableDaemo
     private static final Logger LOGGER = LogManager.getLogger();
 
     /**
-     * Save the list of configService to update automatically
-     */
-    private List<ConfigService> configServiceToUpdate;
-
-    /**
      * 
      * Constructor.
      * 
      * @param configServiceToUpdate_ The list of ConfigService that needs to be refresh automatically.
      */
-    public CatalogAndProductCacheRefreshThread(List<ConfigService> configServiceToUpdate_) {
+    public CatalogAndProductCacheRefreshThread() {
         super(
             "Product and Catalog Cache Thread Daemon",
             BLLManager.getInstance().getConfigManager().getMotuConfig().getDescribeProductCacheRefreshInMilliSec());
-        this.configServiceToUpdate = configServiceToUpdate_;
+        init();
+    }
+
+    public void init() {
+        BLLManager.getInstance().getCatalogManager().getCatalogAndProductCacheManager().updateAllTheCache();
     }
 
     /** {@inheritDoc} */
