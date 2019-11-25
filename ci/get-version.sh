69b1d62e87fd2ca2b2a7263559044c9010842fb4
Jerome Doumerc <jdoumerc@groupcls.com>
2019-11-25 17:06:00 +0100

    Dockering with CI build image and Motu-Product image

---
+++ ci/get-version.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+if [ -z "${CI_PROJECT_DIR}" ]; then
+    CURRENT_DIR=$(cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")" && pwd)
+    CI_PROJECT_DIR=$(dirname "${CURRENT_DIR}")
+else
+    CURRENT_DIR="${CI_PROJECT_DIR}/ci"
+fi
+
+WORKSPACE_DIR="${CI_PROJECT_DIR}"
+
+cat "${WORKSPACE_DIR}/wps-server-parent/pom.xml" | grep -B 1 '<packaging>pom</packaging>' | grep '<version>' | sed 's/^[[:space:]]*<version>//g' | sed 's,</version>[[:space:]]*$,,g'
